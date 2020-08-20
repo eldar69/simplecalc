6398d970783bea24bd58aa0c46798ef85b39021f
eldar69 <58563657+eldar69@users.noreply.github.com>
2020-08-20 21:30:39 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+
+
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Calculator</title>
+    </head>
+    <body>
+    enter values here:
+
+         <form method="post">
+            <input type="text" name="numb1">
+            <input type="text" name="numb2">
+            <select name="operator" id="">
+               <option>None</option>
+               <option>Add</option>
+               <option>Subtract</option>
+               <option>Multiply</option>
+               <option>Divide</option>
+               <option>Square</option>
+            </select>
+            <button type="submit" name="submit" value="submit">Calculate</button>
+         </form>
+
+   answer:
+
+        <?php 
+
+            if (isset($_POST['submit'])) {
+                $result1 = $_POST['numb1'];
+                $result2 = $_POST['numb2'];
+                $operator = $_POST['operator'];
+                switch ($operator) {
+                    case 'None':
+                        echo "You need to select any operator";
+                        break;
+                    case 'Add':
+                        echo $result1 + $result2;
+                        break;
+                    case 'Subtract':
+                        echo $result1 - $result2;
+                        break;
+                    case 'Multiply':
+                        echo $result1 * $result2;
+                        break;
+                    case 'Divide':
+                        echo $result1 / $result2;
+                        break;
+                    case 'Square':
+                        echo $result1 ** $result2;
+                        break;
+                }
+            }
+
+
+         ?>
+        
+
+    </body>
+    </html>
